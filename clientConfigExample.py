8d4f165a67c664bacb6b34e534e53bffd8e7cf36
UnderGCrypto <73098714+UnderGCrypto@users.noreply.github.com>
2024-01-06 00:07:08 +0200

    Update clientConfigExample.py
    the naming was not matching with the ones where it was called.

--- clientConfigExample.py
+++ clientConfigExample.py
@@ -2,7 +2,7 @@
 # # Description: Please add your API key and secret to the clientConfig file.
 # # Description: In the Readme.md file, you will find instructions on how to create an API key and secret.
 
-CLIENT_CONFIG = {
+client_config = {
     'signing_key': '',
     'wallet_address': '',
     'api_key': '',
